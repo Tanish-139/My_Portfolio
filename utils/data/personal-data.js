701d78931dfa7e4ef5cd29d85cabdbaf940ccd07
i_Tanishh <143903784+Tanish-139@users.noreply.github.com>
2025-02-10 19:41:17 +0530

    Update personal-data.js

--- utils/data/personal-data.js
+++ utils/data/personal-data.js
@@ -2,7 +2,7 @@ export const personalData = {
   name: "TANISH KANOJIA",
   profile: '/profile.png',
   designation: "Software Developer",
-  description: "My name is Tanish Kanojia, and I am an aspiring programmer passionate about web application development, particularly in JavaScript. With a self-learning attitude, I enjoy exploring new technologies and tackling problem-solving challenges. I have experience with frameworks like React.js and Next.js, as well as Flask for backend development, and I am also skilled in HTML and CSS. While I am still on my journey to becoming a professional, I am eager to find opportunities that align with my interests and contribute to making the web more accessible.",
+  description: "My name is Tanish Kanojia, and I am an aspiring programmer passionate about web application development, particularly in JavaScript. With a self-learning attitude, I enjoy exploring new technologies and tackling problem-solving challenges. I have experience with frameworks like React.js and Next.js and I am also skilled in HTML and CSS. While I am still on my journey to becoming a professional, I am eager to find opportunities that align with my interests and contribute to making the web more accessible.",
   email: 'tanishkanojia235@gmail.com',
   phone: '+91 9892846067',
   address: 'Mumbai, Maharashtra, India ',
